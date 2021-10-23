4b8d8095333eed78022daa4367592b6f62215a53
Tyler <tyler.wong@ontariotechu.net>
2021-10-23 13:59:33 -0400

    Added camera panning + zoom for GUI

--- Game Engines Assignment 1/Assets/Scripts/CameraDrag.cs
+++ Game Engines Assignment 1/Assets/Scripts/CameraDrag.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraDrag : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float panSpeed = 10f;
+
+    [SerializeField] float zoomSpeed = 10f;
+    [SerializeField] float targetOrtho;
+    [SerializeField] float smoothSpeed = 2.0f;
+    [SerializeField] float minOrtho = 1.0f;
+    [SerializeField] float maxOrtho = 20.0f;
+
     void Start()
     {
-        
+        targetOrtho = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Hold right mouse button to pan camera
+       if (Input.GetMouseButton(1))
+        {
+            var newPosition = new Vector3();
+            newPosition.x = Input.GetAxis("Mouse X") * panSpeed * Time.deltaTime * 25f;
+            newPosition.y = Input.GetAxis("Mouse Y") * panSpeed * Time.deltaTime * 25f;
+
+            transform.Translate(-newPosition);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0.0f)
+        {
+            targetOrtho -= scroll * zoomSpeed * 5f;
+            targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+        }
+
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
     }
 }
