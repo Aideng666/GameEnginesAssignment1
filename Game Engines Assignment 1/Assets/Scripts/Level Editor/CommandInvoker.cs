4fc132f0854a02c1bcc1a9642d26f165c710bdbd
Aiden <aiden.gimpel@ontariotechu.net>
2021-10-24 12:39:25 -0400

    Working Merge

--- Game Engines Assignment 1/Assets/Scripts/Level Editor/CommandInvoker.cs
+++ Game Engines Assignment 1/Assets/Scripts/Level Editor/CommandInvoker.cs
@@ -4,31 +4,16 @@ using UnityEngine;
 
 public class CommandInvoker : MonoBehaviour
 {
-    //static Queue<ICommand> commandBuffer;
     static Queue<Platform> platformCommandBuffer;
 
-    //static List<ICommand> commandHistory;
     static List<Platform> platformHistory;
     static int counter;
 
     private void Awake()
     {
-        //commandBuffer = new Queue<ICommand>();
-        //commandHistory = new List<ICommand>();
-
         platformCommandBuffer = new Queue<Platform>();
         platformHistory = new List<Platform>();
     }
-
-    //public static void AddCommand(ICommand command)
-    //{
-    //    while (commandHistory.Count > counter)
-    //    {
-    //        commandHistory.RemoveAt(counter);
-    //    }
-
-    //    commandBuffer.Enqueue(command);
-    //}
 
     public static void AddCommand(Platform command)
     {
@@ -43,35 +28,6 @@ public class CommandInvoker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (commandBuffer.Count > 0)
-        //{
-        //    ICommand c = commandBuffer.Dequeue();
-        //    c.Execute();
-
-        //    commandHistory.Add(c);
-        //    counter++;
-        //    Debug.Log("Command history length: " + commandHistory.Count);
-        //}
-        //else
-        //{
-        //    if (Input.GetKeyDown(KeyCode.Z))
-        //    {
-        //        if (counter > 0)
-        //        {
-        //            counter--;
-        //            commandHistory[counter].Undo();
-        //        }
-        //    }
-        //    else if (Input.GetKeyDown(KeyCode.R))
-        //    {
-        //        if (counter < commandHistory.Count)
-        //        {
-        //            commandHistory[counter].Execute();
-        //            counter++;
-        //        }
-        //    }
-        //}
-
         if (platformCommandBuffer.Count > 0)
         {
             Platform c = platformCommandBuffer.Dequeue();
@@ -81,36 +37,7 @@ public class CommandInvoker : MonoBehaviour
             counter++;
             Debug.Log("Platform history length: " + platformHistory.Count);
         }
-        //else
-        //{
-        //    if (Input.GetKeyDown(KeyCode.Z))
-        //    {
-        //        if (counter > 0)
-        //        {
-        //            counter--;
-        //            commandHistory[counter].Undo();
-        //        }
-        //    }
-        //    else if (Input.GetKeyDown(KeyCode.R))
-        //    {
-        //        if (counter < commandHistory.Count)
-        //        {
-        //            commandHistory[counter].Execute();
-        //            counter++;
-        //        }
-        //    }
-        //}
-    }
-
-    private void OnEnable()
-    {
-        UndoCommand();
-        RedoCommand();
-    }
-
-    public void UndoCommand()
-    {
-        if (counter > 0)
+        else
         {
             if (Input.GetKeyDown(KeyCode.Z))
             {
@@ -131,12 +58,41 @@ public class CommandInvoker : MonoBehaviour
         }
     }
 
-    public void RedoCommand()
-    {
-        if (counter < commandHistory.Count)
-        {
-            commandHistory[counter].Execute();
-            counter++;
-        }
-    }
+    //private void OnEnable()
+    //{
+    //    UndoCommand();
+    //    RedoCommand();
+    //}
+
+    //public void UndoCommand()
+    //{
+    //    if (counter > 0)
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Z))
+    //        {
+    //            if (counter > 0)
+    //            {
+    //                counter--;
+    //                platformHistory[counter].Undo();
+    //            }
+    //        }
+    //        else if (Input.GetKeyDown(KeyCode.R))
+    //        {
+    //            if (counter < platformHistory.Count)
+    //            {
+    //                platformHistory[counter].Spawn();
+    //                counter++;
+    //            }
+    //        }
+    //    }
+    //}
+
+    //public void RedoCommand()
+    //{
+    //    if (counter < commandHistory.Count)
+    //    {
+    //        commandHistory[counter].Execute();
+    //        counter++;
+    //    }
+    //}
 }
